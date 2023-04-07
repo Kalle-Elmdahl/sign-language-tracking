f6756dcfcd099a4b0d52cb716a378881a3bf1ee8
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2023-04-07 18:38:34 +0200

    Finalize video frames

--- webapp/src/components/HandRecorder.tsx
+++ webapp/src/components/HandRecorder.tsx
@@ -37,9 +37,9 @@ export default function HandRecorder(props: HandRecorderProps) {
     }, 1000)
   }
 
-  const handleUpdateElementVideo: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+  /* const handleUpdateElementVideo: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setSequence
-  }
+  } */
 
   return (
     <>
@@ -47,16 +47,25 @@ export default function HandRecorder(props: HandRecorderProps) {
       {countdown.current >= 0 && <div className="recorder-countdown">{countdown.current}</div>}
       <div className="sequence-previewer">
         <h3>{sequence.name} (Click frame to delete)</h3>
-        {sequence.elements.length === 0 && <p>No frames added to sequence</p>}
         <div>
           <div className="sequence-previewer-list">
             {sequence.elements.map((element, index) =>
               typeof element === "string" ? (
-                <video width={(175 * 16) / 9} height={175}>
+                <video
+                  key={index}
+                  width={(175 * 16) / 9}
+                  height={175}
+                  onClick={() =>
+                    setSequence({
+                      type: "REMOVE_ELEMENT",
+                      payload: { sequence, index },
+                    })
+                  }
+                >
                   <source src={element} type="video/mp4" />
                 </video>
               ) : (
-                <>
+                <div key={index}>
                   <HandPreview
                     key={index}
                     hands={element.hands}
@@ -70,10 +79,27 @@ export default function HandRecorder(props: HandRecorderProps) {
                     }
                   />
                   <p>With video</p>
-                  <input type="text" value={element.video} onChange={handleUpdateElementVideo} />
-                </>
+                  <input type="text" value={element.video} /* onChange={handleUpdateElementVideo} */ />
+                </div>
               )
             )}
+            <form
+              style={{ width: `${(175 * 16) / 9}px` }}
+              onSubmit={(e) => {
+                e.preventDefault()
+                setSequence({
+                  type: "ADD_ELEMENT",
+                  payload: {
+                    sequence,
+                    // @ts-expect-error comment
+                    element: e.target.video.value,
+                  },
+                })
+              }}
+            >
+              <input name="video" type="text" placeholder="Add video to sequence" />
+              <button type="submit">Add</button>
+            </form>
           </div>
         </div>
       </div>
